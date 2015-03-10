2f33cb5f561c2f29b07fd0d742eb3cadaeff7009
aleksikyllonen <kyllonen.aleksi@gmail.com>
2015-03-10 14:53:57 +0200

    Printtaa yolo

--- SnakeSnake/src/peli/Snake.java
+++ SnakeSnake/src/peli/Snake.java
@@ -12,6 +12,7 @@ public class Snake {
 	
 	public Snake(Koordinaatti aloituskohta, int pituus, Suunta suunta){
 		palat = new ArrayList<Pala>();
+		System.out.println("yolo");
 		for(int i=0; i<pituus; i++){
 			
 			Koordinaatti koordinaatti = new Koordinaatti( 
